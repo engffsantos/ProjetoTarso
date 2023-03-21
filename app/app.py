0cf2ba34ce83a8e0654883a56b77c8f79a1aaf28
SamuelOliveira2016 <samu.olive14@gmail.com>
2023-03-21 16:42:57 -0300

    all_view and app updated

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 from app.controllers import booking_controller, evento_controller, local_controller, pessoa_controller
+from app.controllers.booking_controller import booking_controller
+from app.views.evento_view import evento_view
+from app.views.local_view import local_controller
+from app.views.pessoa_view import pessoa_view
+
 
 app = Flask(__name__)
 
@@ -22,6 +27,11 @@ def locais():
 @app.route('/pessoas')
 def pessoas():
     return pessoa_controller.get_all_people()
+
+app.register_blueprint(booking_controller)
+app.register_blueprint(evento_view)
+app.register_blueprint(local_controller)
+app.register_blueprint(pessoa_view)
 
 if __name__ == '__main__':
     app.run()
--- app/views/booking_view.py
+++ app/views/booking_view.py
@@ -1,2 +1,45 @@
-class BookingView:
-    pass
+from flask import Blueprint, render_template, request, redirect, url_for
+from app.models.booking_model import BookingModel
+from app.models.evento_model import EventoModel
+from app.models.local_model import LocalModel
+from app.models.pessoa_model import PessoaModel
+
+booking_view = Blueprint('booking_view', __name__)
+
+@booking_view.route('/reservas')
+def listar_reservas():
+    # Code to retrieve all bookings from the database
+    reservas = BookingModel.listar_todas_reservas()
+    # Render the booking.html template passing the reservas variable as context
+    return render_template('booking.html', reservas=reservas)
+
+@booking_view.route('/reservas/nova', methods=['GET', 'POST'])
+def nova_reserva():
+    eventos = EventoModel.listar_todos_eventos()
+    locais = LocalModel.listar_todos_locais()
+    pessoas = PessoaModel.listar_todas_pessoas()
+    if request.method == 'POST':
+        # Code to create a new booking in the database
+        BookingModel.criar_reserva(request.form)
+        return redirect(url_for('booking_view.listar_reservas'))
+    else:
+        return render_template('booking_form.html', eventos=eventos, locais=locais, pessoas=pessoas)
+
+@booking_view.route('/reservas/editar/<int:id>', methods=['GET', 'POST'])
+def editar_reserva(id):
+    reserva = BookingModel.obter_reserva_por_id(id)
+    eventos = EventoModel.listar_todos_eventos()
+    locais = LocalModel.listar_todos_locais()
+    pessoas = PessoaModel.listar_todas_pessoas()
+    if request.method == 'POST':
+        # Code to update the booking in the database
+        BookingModel.atualizar_reserva(id, request.form)
+        return redirect(url_for('booking_view.listar_reservas'))
+    else:
+        return render_template('booking_form.html', reserva=reserva, eventos=eventos, locais=locais, pessoas=pessoas)
+
+@booking_view.route('/reservas/excluir/<int:id>')
+def excluir_reserva(id):
+    # Code to delete the booking from the database
+    BookingModel.excluir_reserva(id)
+    return redirect(url_for('booking_view.listar_reservas'))
